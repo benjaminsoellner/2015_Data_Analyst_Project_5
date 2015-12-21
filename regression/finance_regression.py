ff4d4c14bab6419ca8d10cbd79fd367f9665c947
Benjamin Söllner <benjamin.soellner@gmail.com>
2015-12-21 14:41:51 +0100

    completed lesson 06 + related mini project

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -21,7 +21,7 @@ dictionary = pickle.load( open("../final_project/final_project_dataset_modified.
 
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
-features_list = ["bonus", "salary"]
+features_list = ["bonus", "salary"] # was: ["bonus", "long_term_incentive"]
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
@@ -29,7 +29,7 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -38,11 +38,19 @@ test_color = "b"
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
 
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
+reg.fit(feature_train, target_train)
 
-
-
-
-
+import pprint
+print "slope: "
+pprint.pprint(reg.coef_)
+print "intercept: "
+pprint.pprint(reg.intercept_)
+print "score on training data: "
+pprint.pprint(reg.score(feature_train, target_train))
+print "score on test data: "
+pprint.pprint(reg.score(feature_test, target_test))
 
 
 ### draw the scatterplot, with color-coded training and testing points
@@ -64,6 +72,14 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+plt.plot(feature, reg.predict(target), color='red', linewidth=3)
+
+# Little hack to remove outliers
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="b")
+print "new slope: "
+pprint.pprint(reg.coef_)
+
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
