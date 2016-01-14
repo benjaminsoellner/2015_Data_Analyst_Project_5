49ec33166b65503205dffdf1420eeed23520de67
Benjamin Söllner <benjamin.soellner@gmail.com>
2016-01-14 01:44:01 +0100

    completed lesson & mini-project 15

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -28,4 +28,38 @@ labels, features = targetFeatureSplit(data)
 
 ### your code goes here 
 
+from sklearn import cross_validation
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
+features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(features, labels, test_size=0.3, random_state=42)
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+acc = accuracy_score(pred, labels_test)
+
+fp = 0
+fn = 0
+tp = 0
+tn = 0
+for i in range(0, len(labels_test), 1):
+    if labels_test[i] == 1.0: # positive
+        if pred[i] == 1.0: # true
+            tp += 1
+        else: # false
+            fp += 1
+    else: # negative
+        if pred[i] != 1.0: # false
+            tn += 1
+        else:
+            fn += 1
+print "FP: ", fp
+print "FN: ", fn
+print "TP: ", tp
+print "TN: ", tn
+print "Accuracy: ", acc
+print "Number of persons predicted as POIs: ", (tp+fp)
+print "Total number of people in test set: ", (tp+fp+tn+fn)
+print "Accuracy if everyone would be predicted '0': ", (tp*1.0)/(tp+fp+tn+fn)
+print "Precision: ", (tp*1.0)/(tp+fp)
+print "Recall: ", (tp*1.0)/(tp+fn)
